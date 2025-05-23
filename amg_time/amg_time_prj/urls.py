3164bc1c4439a099df4ca6fb23486c63ff371f7f
Alex Disgusting <zwergaxt@mail.ru>
2025-05-23 13:37:17 +0300

    [fix] urls

--- amg_time/amg_time_prj/urls.py
+++ amg_time/amg_time_prj/urls.py
@@ -56,7 +56,9 @@ urlpatterns = [
     re_path(r"^api/reports/(?:(?P<len>\w+)/?)?$", views.reports_list),
     re_path(r"^api/reports_d/(\d+)$", views.reports_detail),
     # generic filtered views
+    path(r"api/projects_gen/", views.ProjectList.as_view()),
     path(r"api/contractors_gen/", views.ContractorsList.as_view()),
+    path(r"api/actscontr_gen/", views.ActContractorList.as_view()),
     path(r"api/agreements_gen/", views.AgreementsList.as_view()),
     path(r"api/invoices_gen/", views.InvoicesList.as_view()),
     path(r"api/acts_gen/", views.ActsList.as_view()),
